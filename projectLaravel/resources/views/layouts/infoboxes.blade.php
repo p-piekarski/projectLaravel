dad9c527b47ff27821c28798c92205711c2dd446
Pawel Sydorowicz <pawelsydorowicz@gmail.com>
2021-01-30 02:49:00 +0100

    poprawka w infoboxes

--- projectLaravel/resources/views/layouts/infoboxes.blade.php
+++ projectLaravel/resources/views/layouts/infoboxes.blade.php
@@ -5,7 +5,7 @@
             <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
             <div class="info-box-content">
                 <span class="info-box-text">Stan konta</span>
-                <span class="info-box-number"><small>PLN</small></span>
+                <span class="info-box-number">10578.27 <small>PLN</small></span>
             </div>
         </div>
     </div>
@@ -24,15 +24,6 @@
             </div>
         </div>
     </div>
-    <!-- Historia -->
-    <div class="col-12 col-sm-6 col-md-3">
-        <div class="info-box mb-3">
-            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-            <div class="info-box-content">
-                <a href="/history" class="nav-link">
-                <span class="info-box-text">Historia</span></a>
-            </div>
-        </div>
-    </div>
+
 
 </div>
